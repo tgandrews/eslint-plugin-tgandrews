1f678b549715d8437f0858b985b0296c9eb1c6cc
Tom Andrews <git@tomandrews.co.uk>
2017-04-20 22:07:40 +0100

    Tests are failing

---
+++ test/rules/prefer-separate-exports.js
@@ -0,0 +1,26 @@
+import { RuleTester } from 'eslint';
+import rule from '../../lib/rules/prefer-separate-exports';
+
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module"
+  }
+});
+
+const tester = new RuleTester();
+tester.run('prefer-separate-exports', rule, {
+  valid: [
+    'export default () => {}',
+    'export const foo = () => {}; export const bar = () => {};'
+  ],
+  invalid: [
+    { 
+      code: 'export default { foo: () => {}, bar: () => {} }', 
+      errors: [{
+        message: 'Prefer separate exports over exporting an object',
+        ruleId: 'ExportDefaultDeclaration'
+      }], 
+    },
+  ]
+})
